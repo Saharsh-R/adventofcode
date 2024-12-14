d5a52fad3a710300b6044a7a266bf39829fb41dd
Saharsh Rathi <srathi41@bloomberg.net>
2024-12-14 16:06:58 +0000

    make util functions better to make boiler plate

--- utils/fetch_input_files.py
+++ utils/fetch_input_files.py
@@ -1,6 +1,6 @@
 import os
 from pathlib import Path
-
+from rich import print
 import requests
 from dotenv import load_dotenv
 
@@ -20,12 +20,53 @@ def write_input_file(year: int, day: int):
         verify=False,
     )
     if response.status_code == 200:
-        file_path = parent_dir / f"y{year}" / f"d{day}" / f"d{day}_input.txt"
-        file_path.parent.mkdir(parents=True, exist_ok=True)
+        dev_dir = parent_dir / f"y{year}" / f"d{day}"
+        input_file_path = dev_dir / f"input.txt"
+        code_path = parent_dir / f"test_d{day}_y{year}.py"
+        input_file_path.parent.mkdir(parents=True, exist_ok=True)
 
-        with open(file_path, "w", encoding="utf-8") as file:
+        test_case_file = parent_dir / "1.txt"
+
+        with open(input_file_path, "w", encoding="utf-8") as file:
             file.write(response.text)
             print(f"Data of day {day}, year {year} written to file successfully")
+        if not code_path.exists():
+            with open(code_path, "w", encoding="utf-8") as code_file:
+                code_file.write(f"""# Day {day}, Year {year}
+# put example test cases here in 1.txt in the same folder
+# by default is input.txt
+
+import pytest
+
+from utils.generic_functions import obtain_lines
+
+
+def get_p1(day_input: list[str]):
+    return 111
+
+@pytest.mark.parametrize('day_input, output', [
+    (obtain_lines('1'), 1),
+    (obtain_lines(), 111),
+])
+def test_d{day}_y{year}_p1(day_input: list[str], output: int):
+    assert get_p1(day_input) == output
+
+def get_p2(day_input: list[str]):
+    return 222
+
+@pytest.mark.parametrize('day_input, output', [
+    (obtain_lines('1'), 2),
+    (obtain_lines(), 222),
+])
+def test_d{day}_y{year}_p2(day_input: list[str], output: int):
+    assert get_p2(day_input) == output
+
+                                """)
+
+        open(test_case_file, "a").close()  # create an empty file
+        print(f"Test case file made at {test_case_file}")
+
+        print(f"Code at {code_path}")
     else:
         print(f"Failed to fetch data, status code: {response.status_code}")
         print(f"Error reason: {response.reason}")
--- utils/generic_functions.py
+++ utils/generic_functions.py
@@ -10,6 +10,6 @@ def obtain_lines(file_name: str = "input") -> list[str]:
         inspect.stack()[1].filename
     )  # where this function is being called from
     parent_dir = caller_file.resolve().parent
-    input_file = parent_dir / f"{parent_dir.name}_{file_name}.txt"
+    input_file = parent_dir / f"{file_name}.txt"
     with input_file.open("r", encoding="utf-8") as file:
         return [line.rstrip("\n") for line in file]
